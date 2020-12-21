22ff3e46294cb29f6ec33a40da96b42db1478e9c
sagar <sag9315@gmail.com>
2020-12-21 12:40:04 +0530

    syncing of quote from api on dom on loading page

---
+++ script.js
@@ -0,0 +1,37 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newBtn = document.getElementById("new-quote");
+
+// Get quote from Api
+async function getQuote() {
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    // If author is blank - add unknown
+    if (data.quoteAuthor === "") {
+      authorText.innerText = "Unknown";
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+
+    quoteText.innerText = data.quoteText;
+    // If quote text len>100 => font size lower
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteText.classList.remove("long-quote");
+    }
+  } catch (err) {
+    getQuote();
+    console.log("Whoops! no quote ", err);
+  }
+}
+
+//On load
+getQuote();
